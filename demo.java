c9186b350b061c2fad01f59349c2839f178461d8
kamalhell <kamalsingh055390@gmail.com>
2025-02-20 16:41:56 +0530

    commit

--- demo.java
+++ demo.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
 
 public class demo {
 
-    public static void main(String[] args) {
+                public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
 
-        for(int i=1;i<=10;i++) {
-            System.out.println(i*10);
+                System.out.print("Enter the angle in degrees: ");
+                double degrees = scanner.nextDouble();
+                double radians = Math.toRadians(degrees);
+                double sine = Math.sin(radians);
+                double cosine = Math.cos(radians);
+                double tangent = Math.tan(radians);
+
+                System.out.println("Sine of " + degrees + " degrees: " + sine);
+                System.out.println("Cosine of " + degrees + " degrees: " + cosine);
+                System.out.println("Tangent of " + degrees + " degrees: " + tangent);
+                scanner.close();
+            }
         }
-
-    }
-
-}
