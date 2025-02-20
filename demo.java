d42b70c5cbe11e7579ba3431d1c79476cf6289a3
Kamal Singh <kamalsingh055390@gmail.com>
2025-02-20 16:15:48 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -4,7 +4,7 @@ public class demo {
     public static void main(String[] args) {
 
         for(int i=1;i<=10;i++) {
-            System.out.println(i*2);
+            System.out.println(i*10);
         }
 
     }
