c66a856c810b11ff26f397100fb31b024a8b59e7
kamalhell <kamalsingh055390@gmail.com>
2025-02-20 16:12:47 +0530

    new commit2

--- demo.java
+++ demo.java
@@ -2,7 +2,11 @@
 public class demo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        for(int i=1;i<=10;i++) {
+            System.out.println(i*2);
+        }
+
     }
 
 }
